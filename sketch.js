480e2242b32ce36f750ebf37d6545acbb4cf0b72
lli0o <119279968+lli0o@users.noreply.github.com>
2022-12-12 04:01:29 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,11 +3,13 @@ let button1;
 let button2;
 let input, button;
 let myText;
+let titleImg;
 
 function preload() {
   art = loadStrings('art_termss.csv');
   medium = loadStrings('mediums.csv');
   majors = loadStrings('majors.csv');
+
 }
 
 function setup() {
@@ -18,21 +20,23 @@ function setup() {
   
   statement = createElement('h3','Click to generate thesis ideas.')
   statement.id('myInput');
- 
+
+  //button2 = createElement('button2','❏ Copy Text');
+  //square = createElement('square');
+  //square2 = createElement('square2');
+  //button2.mousePressed(copyTextToClipBoard);
 }
 
 
 function draw() {
   clear();
   //background('#F4E93F');
-  
+ 
   statement.html(' “My thesis explores '+ random(majors) + ' within the realm of visual art. The project explores ' + random(medium) + ', ' + random(medium) + ' through the lens of ' + random(art) + '.”');
   
   
   noLoop();
 }
-
-
 
 
 
